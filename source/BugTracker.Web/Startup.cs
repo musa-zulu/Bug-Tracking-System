c2b5a47eab5b9afddcd2d61d4a4378f52919e688
Musa.Zulu <zuluchs@gmail.com>
2016-04-09 18:37:05 +0200

    first commit

---
+++ source/BugTracker.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugTracker.Web.Startup))]
+namespace BugTracker.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
